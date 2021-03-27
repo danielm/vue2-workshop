83efa6960f543ac75668fae6f83170afd3db94fa
Daniel Morales <daniel85mg@gmail.com>
2021-03-26 22:18:38 -0300

    examples 2

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ new Vue({
   data () {
     return  {
       name: 'Bitcoin',
+      symbol: 'BTC',
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-      changePercent: 10,
+      changePercent: -10,
       
 
       prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
+
+      price: 8200,
+
+      color: 'f4f4f4',
+
+      value: 0,
 
       pricesWithDays: [
         {day: "Monday", value: 8400},
@@ -17,7 +24,37 @@ new Vue({
         {day: "Friday", value: 9400},
         {day: "Saturday", value: 10000},
         {day: "Sunday", value: 10200},
-      ]
+      ],
+
+      showPrices: false,
     };
+  },
+
+  computed: {
+    title () {
+      return `${this.name} - ${this.symbol}`;
+    },
+    convertedValue (){
+      if (!this.value){
+        return 0;
+      }
+
+      return this.value / this.price;
+    }
+  },
+
+  watch: {
+    showPrices (newValue, oldValue) {// name must match the data() property
+      console.log(newValue, oldValue);
+    }
+  },
+
+  methods: {
+    toggleShowPrices () {
+      // this, gives you access to properties and methods within my VUE instance
+      this.showPrices = !this.showPrices;
+
+      this.color = this.color.split('').reverse().join('')
+    }
   }
 });
