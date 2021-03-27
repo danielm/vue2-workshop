287d08f1521d168799e0de385b424a14be2e884f
Daniel Morales <daniel85mg@gmail.com>
2021-03-27 14:42:48 -0300

    Components 1

--- basics/app.js
+++ basics/app.js
@@ -1,52 +1,26 @@
-new Vue({
-  el: '#app',
-  data () {
-    return  {
-      name: 'Bitcoin',
-      symbol: 'BTC',
-      img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-      changePercent: -10,
-      
-
-      prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
-
-      price: 8200,
-
-      color: 'f4f4f4',
-
-      value: 0,
-
-      pricesWithDays: [
-        {day: "Monday", value: 8400},
-        {day: "Tuesday", value: 7900},
-        {day: "Wednesday", value: 8200},
-        {day: "Thursday", value: 9000},
-        {day: "Friday", value: 9400},
-        {day: "Saturday", value: 10000},
-        {day: "Sunday", value: 10200},
-      ],
-
+Vue.component('CoinDetail', {
+  data (){
+    return {
       showPrices: false,
+      value: 0,
     };
   },
 
+  props: [
+    'coin'
+  ],
+
   computed: {
-    title () {
-      return `${this.name} - ${this.symbol}`;
-    },
     convertedValue (){
       if (!this.value){
         return 0;
       }
 
-      return this.value / this.price;
-    }
-  },
-
-  watch: {
-    showPrices (newValue, oldValue) {// name must match the data() property
-      console.log(newValue, oldValue);
-    }
+      return this.value / this.coin.price;
+    },
+    title () {
+      return `${this.coin.name} - ${this.coin.symbol}`;
+    },
   },
 
   methods: {
@@ -54,7 +28,90 @@ new Vue({
       // this, gives you access to properties and methods within my VUE instance
       this.showPrices = !this.showPrices;
 
-      this.color = this.color.split('').reverse().join('')
+      this.$emit('change-color', this.showPrices);
+    }
+  },
+
+  template: `
+    <div>
+      <img v-bind:src="coin.img" v-bind:alt="coin.name" style="width: 50px;">
+
+      <h1 v-bind:class="coin.changePercent > 0 ? 'green' : 'red'">
+        {{ title }}
+        <!-- v-if removes the element from dom if condition is not met -->
+        <span v-if="coin.changePercent > 0">:)</span>
+        <span v-else-if="coin.changePercent < 0">:(</span>
+        <span v-else>:|</span>
+
+        <!-- v-show ads display:non when condition is not met -->
+        <span v-show="coin.changePercent > 0">:)</span>
+        <span v-show="coin.changePercent < 0">:(</span>
+        <span v-show="coin.changePercent == 0">:|</span>
+      </h1>
+
+      <hr />
+
+      <a href="#" v-on:click="toggleShowPrices">{{ showPrices ? 'Hide' : 'Show' }} Prices</a>
+
+      <hr />
+
+      Enter USD: <input type="number" v-model="value">
+      Equals <span>{{ convertedValue }}</span> {{ coin.symbol }}
+
+      <div v-show=showPrices>
+        <ul>
+          <li
+            class="bold"
+            v-for="(p, i) in coin.pricesWithDays"
+            v-bind:key="p.day"
+            v-bind:class="{ orange: p.value == coin.price, red: p.value < coin.price }"
+          >
+            {{ i }} - {{ p.day }} - &#36;{{ p.value }}
+          </li>
+        </ul>
+      </div>
+
+    </div>
+  `
+});
+
+new Vue({
+  el: '#app',
+  data () {
+    return  {
+      btc: {
+        name: 'Bitcoin',
+        symbol: 'BTC',
+        img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+        changePercent: 10,
+        price: 8200,
+        pricesWithDays: [
+          {day: "Monday", value: 8400},
+          {day: "Tuesday", value: 7900},
+          {day: "Wednesday", value: 8200},
+          {day: "Thursday", value: 9000},
+          {day: "Friday", value: 9400},
+          {day: "Saturday", value: 10000},
+          {day: "Sunday", value: 10200},
+        ],
+      },
+      color: 'f4f4f4'
+    };
+  },
+
+  watch: {
+    color (newValue, oldValue) {// name must match the data() property
+      console.log(newValue, oldValue);
+    }
+  },
+
+  methods: {
+    updateColor (status) {
+      if (status){
+        this.color = '4f4f4f';
+      } else {
+        this.color = 'f4f4f4';
+      }
     }
   }
 });
